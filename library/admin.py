a37d13c6c61e867c316ddb7062d5716fdc077ab3
Diyorbek M <backenddevaloper@gmail.com>
2024-04-03 02:29:52 +0500

    relations

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book, Author, BookingBook, Comment
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BookingBook)
+admin.site.register(Comment)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
 
+class Address(models.Model):
+    title = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.title
+
+
+class Role(models.TextChoices):
+    bachelor = ('b', 'Bakalavr')
+    master = ('m', 'Magistr')
+    phd = ('d', 'phd')
+
+
 class User(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     username = models.CharField(max_length=10)
-    password = models.IntegerField()
+    status = models.CharField(max_length=20,choices=Role.choices, default=Role.bachelor)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name
